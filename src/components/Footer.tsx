c272cc95052f97b450f4edb10fba1eafa1b96547
ConfvsioDev <confvsio@gmail.com>
2025-03-14 21:43:26 +0100

    FINAL

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -45,7 +45,7 @@ const Footer: React.FC = () => {
             />
             
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex flex-col md:flex-row justify-between items-center">
+                <div className="flex flex-col md:flex-row justify-around items-center">
                     {/* Logo section with centered spacing */}
                     <div className="space-y-4 flex flex-col items-center mb-6 md:mb-0">
                         <div className="flex items-center space-x-2">
